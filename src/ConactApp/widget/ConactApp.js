81aa2beb2fe1c9a69b7d13ba33c0c9e5e6ab9663
vietduc1441 <vietduc1441@yahoo.com>
2017-08-07 06:24:37 +0200

    review

--- src/ConactApp/widget/ConactApp.js
+++ src/ConactApp/widget/ConactApp.js
@@ -17,7 +17,7 @@ define([
     "ConactApp/lib/jquery-1.11.2",
     "ConactApp/lib/selectize",
     "dojo/text!ConactApp/widget/template/ConactApp.html"
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, selectize, widgetTemplate) {
+], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, _jQuery, selectize, widgetTemplate) {
     "use strict";
 
     var $ = _jQuery.noConflict(true);
@@ -33,23 +33,26 @@ define([
         _handles: null,
         _contextObj: null,
 
-        constructor: function () {
+        constructor: function() {
             this._handles = [];
         },
 
-        postCreate: function () {
+        postCreate: function() {
 
             logger.debug(this.id + ".postCreate");
 
         },
 
-        getCustomerData: function () {
+        getCustomerData: function() {
             mx.data.get({
                 xpath: "//" + this.CustomerEntity,
-                callback: function (objs) {
+                callback: function(objs) {
 
                     let customerList = objs.map(obj => {
-                        return { email: obj.get(this.Email), name: obj.get(this.Name) };
+                        return {
+                            email: obj.get(this.Email),
+                            name: obj.get(this.Name)
+                        };
                     })
                     console.log(customerList);
                     //console.log(objs.map(obj => {obj.jsonData.attributes.Email.value}));
@@ -69,7 +72,7 @@ define([
         //         }
         //     })
         // },
-        getCustomerIntoField: function (customerList) {
+        getCustomerIntoField: function(customerList) {
 
             var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
                 '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
@@ -78,57 +81,55 @@ define([
             //this.inputText.value = "test";
             // this.selectTo.value="test";    
             var preLoadValue = [];
-            var preLoadFromValue = this._contextObj.jsonData.attributes.From.value;
-            var preLoadToValue = this._contextObj.jsonData.attributes.To.value;
+            var preLoadFromValue = this._contextObj.jsonData.attributes.From.value; //use get("attribute")
+            var preLoadToValue = this._contextObj.jsonData.attributes.To.value; //unused variable?
             if (this.maxItems === 1) {
-                preLoadValue.push(this._contextObj.jsonData.attributes.From.value);
+                preLoadValue.push(this._contextObj.jsonData.attributes.From.value); //? From ?
             } else {
-                var toValue = this._contextObj.jsonData.attributes.To.value;
-                if (toValue.indexOf(',') > -1) {
+                var toValue = this._contextObj.jsonData.attributes.To.value; //where "To" come from?
+                if (toValue.indexOf(',') > -1) { //do we need this?
                     preLoadValue = toValue.split(',');
                 } else {
                     preLoadValue.push(toValue);
                 }
-                
+
             }
             var $select = $(this.selectTo).selectize({
-                persist: true,
+                persist: true, //do we need this?
                 maxItems: this.maxItems,
                 valueField: 'email',
                 labelField: 'name',
                 searchField: ['name', 'email'],
                 items: preLoadValue,
-                
                 options: customerList,
                 placeholder: this.placeHolder,
                 hideSelected: true,
-                onChange: function (value) {
+                onChange: function(value) {
                     this._contextObj.set(this.InputField, value);
                     mx.data.commit({
                         mxobj: this._contextObj,
-                        callback: function () {
-                        }
+                        callback: function() {}
                     })
                     mx.data.create({
                         entity: "TestSuite.Invitation",
-                        callback: function (obj) {
+                        callback: function(obj) {
                             console.log("Object created on server")
                             console.log(obj);
                         },
-                        error: function (e) {
+                        error: function(e) {
                             console.log("an error occured" + e);
                         }
                     });
 
                 }.bind(this),
-                render: {
-                    item: function (item, escape) {
+                render: { //how we can make the render customizeable instead of hard code? (user should be able to config this as template in widget)
+                    item: function(item, escape) {
                         return '<div>' +
                             (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
                             (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
                             '</div>';
                     },
-                    option: function (item, escape) {
+                    option: function(item, escape) {
                         var label = item.name || item.email;
                         var caption = item.name ? item.email : null;
                         return '<div>' +
@@ -137,7 +138,7 @@ define([
                             '</div>';
                     }
                 },
-                createFilter: function (input) {
+                createFilter: function(input) { //unused function?
                     var match, regex;
                     // email@address.com
                     regex = new RegExp('^' + REGEX_EMAIL + '$', 'i');
@@ -151,9 +152,11 @@ define([
 
                     return false;
                 },
-                create: function (input) {
+                create: function(input) { //unused funciton?
                     if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
-                        return { email: input };
+                        return {
+                            email: input
+                        };
                     }
                     var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
                     if (match) {
@@ -167,7 +170,7 @@ define([
                 }
             }).bind(this);
         },
-        update: function (obj, callback) {
+        update: function(obj, callback) {
             logger.debug(this.id + ".update");
 
             this._contextObj = obj;
@@ -176,15 +179,15 @@ define([
             this._updateRendering(callback);
         },
 
-        resize: function (box) {
+        resize: function(box) {
             logger.debug(this.id + ".resize");
         },
 
-        uninitialize: function () {
+        uninitialize: function() {
             logger.debug(this.id + ".uninitialize");
         },
 
-        _updateRendering: function (callback) {
+        _updateRendering: function(callback) {
             logger.debug(this.id + "._updateRendering");
 
             if (this._contextObj !== null) {
@@ -197,7 +200,7 @@ define([
         },
 
         // Shorthand for running a microflow
-        _execMf: function (mf, guid, cb) {
+        _execMf: function(mf, guid, cb) {
             logger.debug(this.id + "._execMf");
             if (mf && guid) {
                 mx.ui.action(mf, {
@@ -205,12 +208,12 @@ define([
                         applyto: "selection",
                         guids: [guid]
                     },
-                    callback: lang.hitch(this, function (objs) {
+                    callback: lang.hitch(this, function(objs) {
                         if (cb && typeof cb === "function") {
                             cb(objs);
                         }
                     }),
-                    error: function (error) {
+                    error: function(error) {
                         console.debug(error.description);
                     }
                 }, this);
@@ -218,7 +221,7 @@ define([
         },
 
         // Shorthand for executing a callback, adds logging to your inspector
-        _executeCallback: function (cb, from) {
+        _executeCallback: function(cb, from) {
             logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
             if (cb && typeof cb === "function") {
                 cb();
